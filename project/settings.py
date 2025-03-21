6204ed9e19d831bdf71d2f2bfe91ff186bd4a9b9
Dilmurod <@eshmamatovdilmurod7@gmail.com>
2025-03-21 18:13:38 +0500

    first commit

--- project/settings.py
+++ project/settings.py
@@ -124,4 +124,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
